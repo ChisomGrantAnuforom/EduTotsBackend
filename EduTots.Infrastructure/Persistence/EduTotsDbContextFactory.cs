625e853394cd76c8827551c39ec001049cb72f32
Chisom Anuforom <chisomgrantnufor@gmail.com>
2026-08-17 18:10:06 +0100

    Mask database password in EduTotsDbContextFactory
    Updated database connection string to mask password.

--- EduTots.Infrastructure/Persistence/EduTotsDbContextFactory.cs
+++ EduTots.Infrastructure/Persistence/EduTotsDbContextFactory.cs
@@ -14,7 +14,7 @@ public class EduTotsDbContextFactory : IDesignTimeDbContextFactory<EduTotsDbCont
         var optionsBuilder = new DbContextOptionsBuilder<EduTotsDbContext>();
 
         optionsBuilder.UseSqlServer(
-            "Server=83.147.39.216,59240;Database=EduTots;User Id=sa;Password=Fearofjehovah#1983;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+            "Server=83.147.39.216,59240;Database=EduTots;User Id=sa;Password=###;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
 
         return new EduTotsDbContext(optionsBuilder.Options);
     }
